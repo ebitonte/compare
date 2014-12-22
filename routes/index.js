d650960767b1a8863d497f31ff57a2ebcda12558
Eben Bitonte <ebitonte@mit.edu>
2014-12-22 00:33:41 -0500

    little changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Eben Bitonte' });
+  res.render('index', { title: 'Compare' });
 });
 
 module.exports = router;
