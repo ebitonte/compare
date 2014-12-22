7ddee9e975c0fd363773a460bd0bce32e5f5430c
Eben Bitonte <ebitonte@mit.edu>
2014-12-22 00:15:01 -0500

    initial commit, a little too late perhaps

---
+++ public/javascripts/animations.js
@@ -0,0 +1,6 @@
+$("#header2").hide();
+var controller;
+
+var pin = 0;
+var topofDiv = $("#header").offset().top; //gets offset of header
+var height = $("#header").outerHeight(); //gets height of header
---
+++ public/javascripts/index.js
@@ -0,0 +1,146 @@
+var leftMap;
+var rightMap;
+var locationList;
+
+$(document).ready(function() {
+	console.log('pleeeeease');
+	getLocation(function(location) {
+		console.log('where am i?');
+		var leftOptions = {
+            zoom: 9,
+            center: location
+        };
+        leftMap = new google.maps.Map(document.getElementById('leftMap'),
+            leftOptions);       	
+	})
+	$.ajax({
+		url: 'http://um.media.mit.edu:5005/'
+	})
+	.done(function(result) {
+		locationList = [];
+		result.json_list.forEach(function(i) {
+			var articleWithLocation = articleHasLocation(i);
+			if(articleWithLocation) {
+				locationList.push(articleWithLocation);
+			}
+       	});
+		console.log(locationList);
+		$.cookie('articleNum', 0, {expires: .5});
+       	displayArticle();
+	})
+	.fail(function(err) {
+		console.log(err);
+	});
+});
+
+function getLocation(callback) {
+    var location;
+    navigator.geolocation.getCurrentPosition(function(position) {
+    	console.log('nada');
+        location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        callback(location);
+    });
+}
+
+function articleHasLocation(article) {
+	var hasLocation = false;
+	for(var i = 0; i < article.entities.length; ++i) {
+		if(article.entities[i].type == 'City') {
+			hasLocation = true;
+			article.city = article.entities[i].value;
+			break;
+		}
+	}
+	if(hasLocation) {
+		return article;
+	} else {
+		return null;
+	}
+}
+
+function textSearch(place) {
+	var request = {
+		query: place
+	};
+	service = new google.maps.places.PlacesService(rightMap);
+	service.textSearch(request, function(results, status) {
+       	console.log(status);
+       	console.log(results);
+       	if (status == google.maps.places.PlacesServiceStatus.OK) {
+           	 displayMap(true, results[0]);
+       	}
+    });
+}
+
+/**
+* display map -
+* 	side: 0 = leftSide, 1 = rightSide
+*/
+
+function displayMap(side, location) {
+	console.log(location);
+	console.log(location.geometry.location);
+	var options = {
+		zoom: 9,
+		center: location.geometry.location
+	};
+	if(side) {
+		rightMap = new google.maps.Map(document.getElementById('rightMap'),
+       		options);
+	} else {
+		leftMap = new google.maps.Map(document.getElementById('leftMap'),
+       		options);
+	}
+}
+
+function displayArticle() {
+	var articleNum = $.cookie('articleNum');
+	console.log(articleNum);
+	articleNum = articleNum ? articleNum : 0;
+	console.log(articleNum);
+	var center = new google.maps.LatLng(0,0);
+	var rightOptions = {
+		zoom: 1,
+		center: center
+	};
+	rightMap = new google.maps.Map(document.getElementById('rightMap'),
+    	rightOptions);
+	textSearch(locationList[articleNum].city);
+	if(locationList[articleNum].related.length > 0) {
+		$('#iframe').attr('src', locationList[articleNum].related[0].url);
+	}
+	setArticleTitle(locationList[articleNum]);
+}
+
+function setArticleTitle(article) {
+	$('#article-title').text(article.title);
+}
+
+$('#last').click(function() {
+	var num = $.cookie('articleNum', Number);
+	if(num > 0) {
+		$.cookie('articleNum', num - 1, {expires: .5});
+		displayArticle();
+		$('.arrow.right').addClass('active');
+	} 
+	if(num - 1 == 0) {
+		$('.arrow.left').removeClass('active');
+	}
+});
+
+$('#next').click(function() {
+	console.log('next click');
+	var num = $.cookie('articleNum', Number);
+	console.log(num);
+	if(num < locationList.length) {
+		console.log(locationList.length);
+		$('.arrow.left').addClass('active');
+		$.cookie('articleNum', num + 1, {expires: .5});
+		displayArticle();
+	} 
+	if(num + 1 == locationList.length) {
+		$('.arrow.right').removeClass('active');
+	}
+});
+
+
